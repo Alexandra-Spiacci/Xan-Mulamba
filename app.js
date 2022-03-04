c7fa3f232bd0ae10215f38c33703bbf778bd33ab
XAN <94311985+Alexandra-Spiacci@users.noreply.github.com>
2022-03-04 13:23:12 -0300

    V1
    Criando a estrutura.

--- app.js
+++ app.js
@@ -13,10 +13,45 @@ images.forEach((image, idx) => {
     image.style.backgroundImage = `url(./images/${idx + 1}.png)`
 })
 
-/*
+
 items.forEach(item => {
     let clone = item.cloneNode(true);
     clone.classList.add('clone');
     slider.appendChild(clone);
     clones.push(clone);
-}) */
+}) 
+
+function getClonesWidth(){
+    let width = 0;
+    clones.forEach(clone => {
+        width += clone.offserWidth;
+
+    })
+    return width;
+}
+function getScrollPos(){
+    return window.scrollY;
+} 
+
+function ScrollUpdate(){
+    scrollPos = getScrollPos();
+    if(clonesWidth+scrollPos >= sliderWidth){
+        window.scrollTo({top: 1});
+    }else if (scrollPos <= 0){
+        window.scrollTo({top: sliderWidth-clonesWidth-1})
+    }
+    slider.style.transform = `translateX(${-window.scrollY}px)`
+    requestAnimationFrame(ScrollUpdate)
+}
+function onLoad(){
+    calculateDimensions()
+    document.body.style.height = `${sliderWidth}px`
+    window.scrollTo({top: 1});
+    ScrollUpdate();
+}
+function calculateDimensions(){
+
+    sliderWidth=slider.getBoundingClientRect().width;
+    clonesWidth=getClonesWidth();
+}
+onLoad()
